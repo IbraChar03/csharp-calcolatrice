5e48feb5d2e7c9cf80357e2e6ba2b0858a1efe35
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-12 14:11:49 +0200

    somma numeri

--- csharp-calcolatrice/Program.cs
+++ csharp-calcolatrice/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(CalcoliHelper.SommaNumeri(2, 3));
         }
     }
 }
