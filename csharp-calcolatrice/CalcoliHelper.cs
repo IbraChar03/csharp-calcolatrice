3d6330a0e49f8a1e9f365c50b3947a5ed06e1497
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-12 14:46:06 +0200

    elevamento a potenza

--- csharp-calcolatrice/CalcoliHelper.cs
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -63,5 +63,18 @@ namespace csharp_calcolatrice
             return Math.Max(num1, num2);
         }
         //overloading
+        public static double ElevamentoPotenza(int num1,int num2)
+        {
+            if(num1 == 0)
+            {
+                num1 = 1;
+            }
+            else if(num2 == 0)
+            {
+                num2 = 1;
+            }
+            return Math.Pow(num1,num2);
+        }
+       
     }
 }
