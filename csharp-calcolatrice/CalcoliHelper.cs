4b5782700776ff2da83d5f400c97fdfbf4959fae
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-12 14:47:02 +0200

    condizione if

--- csharp-calcolatrice/CalcoliHelper.cs
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -66,13 +66,11 @@ namespace csharp_calcolatrice
         public static double ElevamentoPotenza(int num1,int num2)
         {
             if(num1 == 0)
-            {
-                num1 = 1;
-            }
+             num1 = 1;
+            
             else if(num2 == 0)
-            {
-                num2 = 1;
-            }
+             num2 = 1;
+            
             return Math.Pow(num1,num2);
         }
        
