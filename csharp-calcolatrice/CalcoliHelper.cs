bae969ff4614b23706fd9c38119912fd1a88dc48
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-12 14:11:07 +0200

    Add project files.

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    internal class CalcoliHelper
+    {
+        public static int SommaNumeri(int num1, int num2)
+        {
+            int somma =  num1 + num2;
+            return somma;
+        }
+    }
+}
